cfd66e1d55f38950a67604f1ea6d3b16918047b2
Sean Kim <seankimsdet@gmail.com>
2019-10-17 11:31:26 -0500

    initial checkin

---
+++ src/test/java/Smoke.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class Smoke {
+
+    @DisplayName("Sample test")
+    @Test
+    public void Test1() {
+        System.out.println("Test123");
+    }
+
+
+}
